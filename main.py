556dee0a03e881d2c8974f59daa52eac3b7c26e1
eblan3424gf <145856094+eblan3424gf@users.noreply.github.com>
2023-09-30 18:38:40 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,33 @@
+print("Hi kiberOne"cccc)
+str - "22"
+print(type(str1))
+int1 = 2
+print(type(int1))
+float = 3.5
+print(type(float1))
+1 = [1,2,3,4]
+print(type(1))
+
+print(type(1+2.0)
+      
+print(type(type(2.4+1)))
+
+print(type(2.0 * 1))
+
+Jon_age = 20
+mom_age= 46
+paul = (mom_age + Jon_age)/6
+print(paul))
+
+def imya_func(a,b):
+    return 5 * x
+
+print(my_function(5))
+print(my_function(9))
+
+1 = [1,2,3,4,4,4]
+print("6")
+
+
+
+
